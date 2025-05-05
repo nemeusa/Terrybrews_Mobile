bffd7e3e286af21966a073d289a4ca79f2ea23dc
nemeusa <nemeusa@gmail.com>
2025-05-05 14:10:55 -0300

    Remote Config
    Dios no existe. Hoy lo aprendí

--- Assets/RemoteConfig.cs
+++ Assets/RemoteConfig.cs
@@ -4,10 +4,24 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
 
-public class ExampleSample : MonoBehaviour
+public class RemoteConfigStart : MonoBehaviour
 {
+    public static RemoteConfigStart Instance { get; private set; }
+
     public struct userAttributes { }
     public struct appAttributes { }
+
+    public bool ndGenerator;
+    public int BreakChance;
+    public int ItemSpawnMin;
+    public float ClientSpeed;
+    public float Time;
+    public int ItemSpawnMax;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     async Task InitializeRemoteConfigAsync()
     {
@@ -34,8 +48,21 @@ public class ExampleSample : MonoBehaviour
         RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
     }
 
+    public void Fetch()
+    {
+        if (Utilities.CheckForInternetConnection())
+            RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
+    }
+
     void ApplyRemoteSettings(ConfigResponse configResponse)
     {
         Debug.Log("RemoteConfigService.Instance.appConfig fetched: " + RemoteConfigService.Instance.appConfig.config.ToString());
+
+        ndGenerator = RemoteConfigService.Instance.appConfig.GetBool("ndGenerator");
+        BreakChance = RemoteConfigService.Instance.appConfig.GetInt("BreakChance");
+        ItemSpawnMin = RemoteConfigService.Instance.appConfig.GetInt("ItemSpawnMin");
+        ClientSpeed = RemoteConfigService.Instance.appConfig.GetFloat("ClientSpeed");
+        Time = RemoteConfigService.Instance.appConfig.GetFloat("Time");
+        ItemSpawnMax = RemoteConfigService.Instance.appConfig.GetInt("ItemSpawnMax");
     }
-}  
+}
